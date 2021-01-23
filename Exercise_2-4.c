03bc60cf1a4900575f7be16e9ae0df495ce0e069
CASIO15 <71800501+CASIO15@users.noreply.github.com>
2021-01-23 22:01:02 +0200

    Update Exercise_2-4.c

--- Exercise_2-4.c
+++ Exercise_2-4.c
@@ -1,37 +1,3 @@
-/*
-#include <stdio.h>
-#define MAXSIZE 100
-
-int any(const char s1[], const char s2[]);
-
-int main(void)
-{
-    char s1[MAXSIZE] = "01234567890123456789z";
-    char s2[MAXSIZE] = "abcdefghijklmnopqrstuvwxyz";
-
-    printf("Reuslt: %d\n", any(s1, s2));
-    return 0;
-}
-
-int any(const char s1[], const char s2[])
-{
-    int i, j;
-    i = j = 0;
-
-    while (s1[i] != '\0') {
-        if (s1[i] == s2[j]) {
-            return i;
-        } else {
-            if (s2[j] == '\0') {
-                j = 0;
-                i++;
-            }
-        }
-        j++;
-    }
-    return -1;
-}
-*/
 #include <stdio.h>
 #define MAXSIZE 1000
 

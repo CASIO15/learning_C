335a9b1abc031387440d5546d5b4f88d02f30a14
CASIO15 <71800501+CASIO15@users.noreply.github.com>
2021-01-25 20:35:48 +0200

    Update Exercise_2-4.c

--- Exercise_2-4.c
+++ Exercise_2-4.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-#define MAXSIZE 1000
 
-void squeeze(char s1[], char s2[]);
+#define MAXSTR 1000
+
+void squeeze(char str1[], char str2[]);
 
 int main(void)
 {
-    char s1[MAXSIZE] = "abcd0000efg";
-    char s2[MAXSIZE] = "0000000000abcd";
+    char str1[MAXSTR] = ")hello world :)";
+    char str2[MAXSTR] = ":)aeiou";
 
-    squeeze(s1, s2);
-    printf("Reuslt: %s\n", s1);
+    squeeze(str1, str2);
+
+    printf("Result: %s", str1);
+
     return 0;
 }
 
@@ -17,12 +20,18 @@ void squeeze(char s1[], char s2[])
 {
     int i, j, k;
 
-    for (i=0; s2[i] != '\0'; i++) {
-        for (j=k=0; s1[j] != '\0'; j++){
-            if (s1[j] == s2[i]){
-            } else
-                s1[k++] = s1[j];
+    k = 0;
+    j = 0;
+
+    for(i=0; s1[i] != '\0';){
+        if (s2[j] == '\0'){
+            s1[k++] = s1[i];
+            i++;
+            j = 0;
+        } if (s1[i] == s2[j]) {
+            ++i;
         }
-        s1[k] = '\0';
+        j++;
     }
+    s1[k] = '\0';
 }

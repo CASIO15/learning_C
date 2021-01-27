9eb46d875df13c5d2e8e7810fc4fa216d9aaa80d
CASIO15 <71800501+CASIO15@users.noreply.github.com>
2021-01-27 22:10:25 +0200

    Create Exercise_2-7.c

---
+++ Exercise_2-7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c\n"
+#define BYTE_TO_BINARY(byte)   \
+  (byte & 128 ? '1' : '0'),    \
+      (byte & 64 ? '1' : '0'), \
+      (byte & 32 ? '1' : '0'), \
+      (byte & 16 ? '1' : '0'), \
+      (byte & 8 ? '1' : '0'),  \
+      (byte & 4 ? '1' : '0'),  \
+      (byte & 2 ? '1' : '0'),  \
+      (byte & 1 ? '1' : '0')
+
+unsigned invert(int x, int p, int n);
+
+int main()
+{
+    unsigned int x = 0b11010111;
+
+    printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(invert(x, 1, 4)));
+    return 0;
+}
+
+unsigned invert(int x, int p, int n)
+{
+    p++;
+    unsigned mask_x;
+    mask_x = ~(~0 << n) << p;
+    return mask_x ^ x;
+
+}

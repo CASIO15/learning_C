ff93e30f34ab41fe9814c16145a9401219dd5a24
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-02-03 21:25:19 +0200

    Update Exercise_3-1.c

--- Exercise_3-1.c
+++ Exercise_3-1.c
@@ -5,47 +5,56 @@ int binsearch2(int x, int v[], int n);
 
 int main()
 {
-    int arr[200000], i, res, res2;
-    for (i = 0; i <= 100000; i++){
+    int arr[20000], i, res, res2;
+    for (i = 0; i <= 10000; i++){
         arr[i] = i;
     }
 
-    res = binsearch(15450, arr, 100000);
-    res2 = binsearch2(15450, arr, 100000);
-    
+    res = binsearch(5005, arr, 10000);
+    res2 = binsearch2(5005, arr, 10000);
+
     printf("Test outside: %d  Test inside: %d\n", res, res2);
 }
 
 int binsearch(int x, int v[], int n) {
-    int high, mid, low;
+    int high, mid, low, count;
+    count = 0;
     low = 0;
     high = n - 1;
 
     while (low <= high) {
+        count += 1;
         mid = (low + high) / 2;
         x > v[mid] ? (low = mid + 1) : (high = mid - 1);
+        printf("%d  %d\n", count, mid);
     }
 
-    if (v[mid] == x)
-        return mid;
-    else
+    if (low == x) {
+        return low;
+    }
+    else {
         return -1;
+    }
 }
 
 
 int binsearch2(int x, int v[], int n)
 {
-    int high, low, mid;
+    int high, low, mid, count;
     low = 0;
     high = n-1;
+    count = 0;
 
+    printf("\n");
     while (low <= high){
+        count += 1;
         mid = (low + high) / 2;
 
+        printf("%d  %d\n", count, mid);
         if (x > v[mid]){
             low = mid + 1;
         } else if (x < v[mid]){
-            high = mid + 1;
+            high = mid - 1;
         } else
             return mid;
     }

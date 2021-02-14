a7ccf7d2ffb273f4957c84ac8c59e49147ad4160
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-02-14 20:53:15 +0200

    Update Exercise_4-7.c

--- Exercise_4-7.c
+++ Exercise_4-7.c
@@ -14,9 +14,10 @@ int getch(void);
 int main(void)
 {
     int i = 0;
-    char s[MAXSIZE] = "4444";
+    char s[MAXSIZE] = "tomer";
     ungets(s);
 
+    // putting the chars from getch to the input
     while (i < MAXSIZE) {
         putchar(getch());
         i++;
@@ -24,7 +25,7 @@ int main(void)
     return 0;
 }
 
-
+// filling the buffer of ungetch.
 void ungets(char s[])
 {
     int len = strlen(s);
@@ -33,11 +34,14 @@ void ungets(char s[])
         ungetch(s[--len]);
 }
 
+// when the buff_pos is greater than zero, it returns the buff elements in reverse, so the reversed string of
+// ungetch becomes regular again
 int getch(void)
 {
     return (buff_pos > 0) ? buff[--buff_pos] : getchar();
 }
 
+// ungetch gets the letters reversed and inserts them to the buffer
 void ungetch(int c)
 {
     if (buff_pos >= MAXSIZE)

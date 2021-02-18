609322d53031b1b5c3ce4626441a107e4c14b4c7
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-02-18 19:27:32 +0200

    Update Exercise_4-12.c

--- Exercise_4-12.c
+++ Exercise_4-12.c
@@ -1,31 +1,39 @@
 #include <stdio.h>
-
 #define MAXSIZE 100
+
 void recursive_itoa(int n, char s[]);
-static int counter = 0;
 
 int main()
 {
-    int n = -144540;
     char s[MAXSIZE];
-    
-    recursive_itoa(n ,s);
-    s[counter] = '\0';
 
-    printf("%s\n", s);
+    recursive_itoa(826, s);
+    printf("%d is converted to %s.\n", 826, s);
+    recursive_itoa(-2093, s);
+    printf("%d is converted to %s.\n", -2093, s);
+
     return 0;
 }
 
 
 void recursive_itoa(int n, char s[])
 {
+    static int counter = 0, call = 0;
+    call++;
+
     if (n < 0) {
         s[counter++] = '-';
         n = -n;
     }
-    if (n/10)
-        recursive_itoa(n / 10, s);
 
+    if (n/10) {
+        recursive_itoa(n / 10, s);
+    }
 
     s[counter++] = (char) (n % 10 + '0');
+    s[counter] = '\0';
+
+    call--;
+    if (call == 0)
+        counter = 0;
 }

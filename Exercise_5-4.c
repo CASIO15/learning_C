e5cbe78873256bba7bbd41f632ef55373354d386
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-02-22 19:17:22 +0200

    Update Exercise_5-4.c

--- Exercise_5-4.c
+++ Exercise_5-4.c
@@ -8,22 +8,22 @@ int strend_1(char *s, char *t)
 {
     int counter = 0;
     unsigned int t_length = strlen(t);
-    
+
     // iterating over s and t, until they both points to \0
     while (*s++ && *t++) {
-        if (*s == '\0' && *t != '\0')
+        if (!*s && *t)
             s--;
-        else if (*t == '\0' && *s != '\0')
+        else if (!*t && *s)
             t--;
         else if (!*s && !*t)
             break;
     }
-    
+
     // iterating backwards and checking the equality of each char.
     while(*--s == *--t)
         // updating counter
         counter++;
-    
+
     // if the counter is equal to the length of t, return 1
     if (counter == t_length)
         return 1;
@@ -33,7 +33,7 @@ int strend_1(char *s, char *t)
 
 int main()
 {
-    char s[] = "hello world", t[] = "world";
+    char s[] = "hello world", t[] = " world";
 
     printf("%d\n", strend_1(s, t));
     return 0;

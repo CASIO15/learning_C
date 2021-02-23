ecc508afbfcce981254b0efd56a1873270d37082
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-02-23 18:30:38 +0200

    Create strncmp.c

---
+++ Exercise_5-4/strncmp.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+
+
+int strncmp(char *s, char *t, int n);
+
+int strncmp(char *s, char *t, int n)
+{
+    while (*s++ && *t++ && (--n > 0)) {
+        if (*s == *t)
+            continue;
+        if (*s != *t)
+            return *s - *t;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    char *s = "Hello world", *t = "Hello world !";
+    int x = strncmp(s, t, 7);
+
+    (x == 0) ? puts("Strings are equal.") : puts("Strings are not equal.");
+    return 0;
+}

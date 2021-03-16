3379145192a536d7f23f973deb24ee4f2b4566ae
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-03-16 10:58:00 +0200

    Update recursive_cosine.c

--- Udemy_data_structures_exercise/recursive_cosine.c
+++ Udemy_data_structures_exercise/recursive_cosine.c
@@ -8,7 +8,7 @@ double cosine_series(int n, int count);
 
 double cosine_series(int n, int count)
 {
-    static double pow=1, fact=1, sign = 1.0;
+    static double pow=1.0, fact=1.0, sign = 1.0;
     double r;
 
     if (count == 0)
@@ -18,7 +18,7 @@ double cosine_series(int n, int count)
         r = cos_series(n, count-2); // head recursion, so all calculation are done at return time.
         pow *= (n*n); // calculating power of n
         fact *= (count * (count-1)); // calculating factorial 
-        sign *= -1;
+        sign *= -1.0;
         return sign * ((r*sign) + (pow / fact));
     }
 }
